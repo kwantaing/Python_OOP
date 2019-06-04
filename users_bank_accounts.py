935b6c230791737306eaa2d99910ae51a68ba066
Quentin Ng <qng1997@gmail.com>
2019-06-04 14:55:43 -0700

    added modularizing

--- users_bank_accounts.py
+++ users_bank_accounts.py
@@ -1,27 +1,3 @@
-class User:
-    def __init__(self,name,email,balance=0):
-        self.name = name
-        self.email = email
-        self.account_balance = 0
-    
-    def make_withdrawal(self,amount):
-        self.account_balance -= amount
-        return self
-    
-    def display_user_balance(self):
-        print(f"User: {self.name} , Balance: ${self.account_balance}")
-    
-    def make_deposit(self,amount):
-        self.account_balance += amount
-        return self
-
-    def transfer_money(self,other_user,amount):
-        self.account_balance-=amount
-        other_user.account_balance+=amount
-        print(self.name,"'s account balance: $",self.account_balance)
-        print(other_user.name,"'s account balance: $",other_user.account_balance)
-        return self
-
 class BankAccount:
     def __init__(self,int_rate, balance):
         self.int_rate = int_rate
@@ -46,3 +22,32 @@ class BankAccount:
         self.balance+=(self.balance*self.int_rate)
         print("Balance: $",self.balance)
         return self
+
+
+class User:
+    def __init__(self,name,email,balance=0):
+        self.name = name
+        self.email = email
+        self.account =BankAccount(int_rate = 0.02, balance=0)
+    
+    def make_withdrawal(self,amount):
+        self.account.withdraw(amount)
+        return self
+    
+    def display_user_balance(self):
+        print(f"User: {self.name} , Balance: ${self.account.balance}")
+    
+    def make_deposit(self,amount):
+        self.account.deposit(amount)
+        return self
+
+    def transfer_money(self,other_user,amount):
+        self.account.balance-=amount
+        other_user.account.balance+=amount
+        print(self.name,"'s account balance: $",self.account.balance)
+        print(other_user.name,"'s account balance: $",other_user.account.balance)
+        return self
+
+James = User("James","james@email.com")
+James.make_deposit(40)  #James.account.Deposit(40) accomplishes the same thing
+James.account.display_account_info()
